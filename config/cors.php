5432b1fc16e78e0c93db2bc2e27aab9df580080f
marelly-colla <marelly.colla@upch.pe>
2025-11-29 11:50:51 -0500

    fix: patterns para deploys en vercel

--- config/cors.php
+++ config/cors.php
@@ -3,19 +3,15 @@
 return [
 
     'paths' => ['api/*'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => ['https://trouvels360.vercel.app'],
-
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins_patterns' => [
+        'https://trouvels360.vercel.app',       
+        'https://trouvels360-.*\.vercel\.app',
+    ],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
